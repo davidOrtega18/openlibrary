ab7bde38780118dc5869ac1b166690d986c10cce
NATrimmer <nicholasatrimmer@gmail.com>
2024-02-28 13:44:12 -0800

    Moved inline js to seperate module

---
+++ openlibrary/plugins/openlibrary/js/loans/index.js
@@ -0,0 +1,11 @@
+export function initLoans() {
+  window.q.push(function() {
+    $("a.leave").on('click', function() {
+      var title = $(this).parents("tr").find(".book").text();
+      $("#leave-waitinglist-dialog strong").text(title);
+      $("#leave-waitinglist-dialog")
+          .data("origin", $(this))
+          .dialog("open");
+    });
+  });
+}
